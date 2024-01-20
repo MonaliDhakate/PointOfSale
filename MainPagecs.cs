b4bb32c18488b6c7e3214e8f33ee7f81ecf7c870
MonaliDhakate <143076144+MonaliDhakate@users.noreply.github.com>
2024-01-20 11:59:29 +0530

    Add files via upload

---
+++ MainPagecs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PointOfSale
+{
+    public partial class MainPagecs : Form
+    {
+        public MainPagecs()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            AddCustomer a = new AddCustomer();
+            a.ShowDialog();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            AddInventory a = new AddInventory();
+            a.ShowDialog();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            AddToCart a = new AddToCart();
+            a.ShowDialog();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            Payment p = new Payment();
+            p.ShowDialog();
+        }
+    }
+}
