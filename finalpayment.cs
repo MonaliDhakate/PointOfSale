a3e348408759203c7dfd657c6aa1fe124bb87426
MonaliDhakate <143076144+MonaliDhakate@users.noreply.github.com>
2024-01-20 11:53:12 +0530

    Add files via upload

---
+++ finalpayment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale
+{
+    public class finalpayment
+    {
+        [Key]
+        [Required]// does not blank
+        [DisplayName("ID")]
+        public int id { get; set; }
+
+        [Required]
+        [DisplayName("CustomerName")]
+        public string? customername { get; set; }  //? is for null value is allowed
+
+        [Required]
+        [DisplayName("productname")]
+        public string? productname { get; set; }
+
+        [Required]
+        [DisplayName("totalamount")]
+        public double? totalamount { get; set; }
+
+        [Required]
+        [DisplayName("discount")]
+        public int? discount { get; set; }
+
+        [Required]
+        [DisplayName("date")]
+        public DateTime? date { get; set; }
+
+
+
+
+    }
+}
