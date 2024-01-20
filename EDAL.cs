fa34f3e051dd2e26d6d3d9fe5d07ef40ee1acd27
MonaliDhakate <143076144+MonaliDhakate@users.noreply.github.com>
2024-01-20 11:58:05 +0530

    Add files via upload

---
+++ EDAL.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PointOfSale
+{
+    public class EDAL:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string ConnectionString = ConfigurationManager.AppSettings["SqlConnectionString"];
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+        public DbSet<finalpayment> payments { get; set; }
+    }
+}
